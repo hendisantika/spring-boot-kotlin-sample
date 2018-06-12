a58d91bdd3c8b9953d71114f27be8b1ac04b2f1e
Hendi Santika <hendisantika@gmail.com>
2018-06-12 08:44:42 +0700

    #3 - Add UserNotFound Exception

---
+++ src/main/kotlin/com/hendisantika/springbootkotlinsample/account/UserNotFoundException.kt
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootkotlinsample.account
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-kotlin-sample
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 12/06/18
+ * Time: 08.44
+ * To change this template use File | Settings | File Templates.
+ */
+open class UserNotFoundException(message: String) : RuntimeException(message) {}
