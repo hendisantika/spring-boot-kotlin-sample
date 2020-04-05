6b51d5c907e40ee19c9fe3851ce842d0b573c6d0
Hendi Santika <hendisantika@yahoo.co.id>
2020-04-06 06:44:19 +0700

    #75 - Bug Fix error

--- src/main/kotlin/com/hendisantika/springbootkotlinsample/message/MessageController.kt
+++ src/main/kotlin/com/hendisantika/springbootkotlinsample/message/MessageController.kt
@@ -5,11 +5,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.validation.BindingResult
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.ModelAttribute
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.*
 import javax.validation.Valid
 
 /**
@@ -23,7 +19,7 @@ import javax.validation.Valid
  * To change this template use File | Settings | File Templates.
  */
 @Controller
-@RequestMapping("/message")
+@RequestMapping("message")
 class MessageController(val messageService: MessageService) {
 
     @ModelAttribute("messages")
